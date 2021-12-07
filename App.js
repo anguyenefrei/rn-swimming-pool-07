a18fb81d29aafd3c27343a301cb97d0313095303
Amelie NGUYEN <amelienguyen@MacBook-Pro-de-Reset.local>
2021-12-07 23:08:09 +0100

    done

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const First = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{color: 'blue'}} onPress={() => navigation.navigate('Second')}>First Page</Text>
+      
+    </View>
+  );
+}
+const Second = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{color: 'blue'}} onPress={() => navigation.navigate('First')}>Second Page</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator(); 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="First" component={First} />
+        <Stack.Screen name="Second" component={Second} />
+      </Stack.Navigator>
+    </NavigationContainer>
     </View>
   );
 }
